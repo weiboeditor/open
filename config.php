1c008a4243f6926dd8ae6009505d0929b5095788
FlashSoft <flashsoft@126.com>
2016-06-22 16:25:45 +0800

    初始化Demo
    Signed-off-by: FlashSoft <flashsoft@126.com>

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , 'XXXXXXXX' );
+define( "WB_SKEY" , 'XXXXXXXXXXXXXXXXXX' );
+define( "WB_CALLBACK_URL" , 'http://XXXXXXXXXX/callback.php' );
